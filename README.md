db57a2fd760d3e603f7c091551d15b132435a1e2
David Ford <djfordz@gmail.com>
2018-10-09 04:35:59 -0500

    fixed script to uncomment proper dir in http.conf and added supervisord dir to work on centOS7

--- README.md
+++ README.md
@@ -4,32 +4,28 @@
 
 Varnish and Supervisord need to be installed first.
 
+### Install nDeploy plugin
+
+Install [Autom8n](https://www.autom8n.com/) but it is not free.
+
 ### For Varnish
 
 Follow the docs here:
 [Install Varnish 4.1.9](https://packagecloud.io/varnishcache/varnish41/install#manual-rpm)
 
-### For Supervisord
+### For Supervisord on CENTOS6
 
 Follow the docs here:
 [Install Supervisord 3.3.3](http://supervisord.org/installing.html)
 
-Note:: Plugin has only been tested with Varnish version 4.1.9 and Supervisord version 3.3.3
+### For Supervisord on CENTOS7
 
-If you use any other versions, please let me know if it works or not by submitting an issue.
-
-### If you wish to use Varnish over https you must install an Nginx Reverse Proxy
-
-We recommend using [Autom8n](https://www.autom8n.com/) but it is not free.
-This is what this plugin is tested with, if you use any other nginx reverse proxy, let us know if it works.
+`yum install supervisord`
 
 ### Install Cpanel Varnish Plugin
 
 run the install.sh script as root. (NOTE: Cpanel plugins can only be installed by root user)
 `./install.sh`
-
-In http.conf, there is one thing you must do:
-uncomment/add this line to end of every `<VirtualHost>`
 
 ## Usage
 
--- fix_httpd.sh
+++ fix_httpd.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo "Backing up /etc/apache2/conf/http.conf to /etc/apache2/conf/http.conf.bakvz"
+echo "Changing Apache to Listen on 127.0.0.1:8080"
+
 _TEXT_TO_FIND="Listen 0.0.0.0"
 
 _BEST_PATH_LINE_NUMBER=$(grep -n "${_TEXT_TO_FIND}" /etc/apache2/conf/httpd.conf | head -1 | cut -d: -f1)
@@ -8,3 +11,27 @@ _TOTAL_LINES=$( wc -l /etc/apache2/conf/httpd.conf | cut -c -3)
 if [[ $_LINE_TO_EDIT -lt $_TOTAL_LINES ]]; then
     sed -i.bakvz "${_LINE_TO_EDIT}i\Listen 127\.0\.0\.1\:8080\n" /etc/apache2/conf/httpd.conf
 fi
+
+
+echo "Backing up /etc/apache2/conf/http.conf to /etc/apache2/conf/http.conf.bakvz1"
+echo "Uncommented userdata conf dir"
+
+sed -i.bakvz1 -e 's/# Include "\/etc\/apache2\/conf.d\/userdata\/std\/2_4\//Include "\/etc\/apache2\/conf.d\/userdata\/std\/2_4\//g' /etc/apache2/conf/httpd.conf
+sed -i.bakvz1 -e 's/# Include "\/etc\/apache2\/conf.d\/userdata\/ssl\/2_4\//Include "\/etc\/apache2\/conf.d\/userdata\/ssl\/2_4\//g' /etc/apache2/conf/httpd.conf
+
+
+echo "touching varnish.conf in virtualhost userdata"
+IFS=$'\n' GLOBIGNORE='*' command eval  '_DOMAINS=($(cat /etc/trueuserdomains | cut -d: -f1))'
+IFS=$'\n' GLOBIGNORE='*' command eval  '_USERS=($(cat /etc/trueuserdomains | cut -d: -f2))'
+_tLEN=${#_USERS[@]}
+
+for (( i=0; i<${_tLEN}; i++ ));
+do
+    USER=$(echo ${_USERS[$i]} | sed -e 's/  *$//')
+    DOMAIN=$(echo ${_DOMAINS[$i]} | sed -e 's/ *$//')
+    mkdir -p /etc/apache2/conf.d/userdata/std/2_4/$USER/$DOMAIN/
+    touch /etc/apache2/conf.d/userdata/std/2_4/$USER/$DOMAIN/varnish.conf
+    mkdir -p /etc/apache2/conf.d/userdata/ssl/2_4/$USER/$DOMAIN/
+    touch /etc/apache2/conf.d/userdata/ssl/2_4/$USER/$DOMAIN/varnish.conf
+done
+
